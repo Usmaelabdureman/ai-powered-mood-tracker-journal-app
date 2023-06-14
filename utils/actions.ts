af19addc462ed2959b9cd92d32d4658cb3eabcec
Usmaelabdureman <uabdureman@gmail.com>
2023-06-14 11:22:54 +0300

    update paths update

--- utils/actions.ts
+++ utils/actions.ts
@@ -2,4 +2,9 @@
 
 import { revalidatePath } from 'next/cache'
 
-export const update = (paths = []) => paths.forEach((p) => revalidatePath(p))
+// export const update = (paths = []) => paths.forEach((p) => revalidatePath(p))
+
+export const update = (paths: string[] = []) => {
+    paths.forEach((p: string) => revalidatePath(p));
+  };
+  
